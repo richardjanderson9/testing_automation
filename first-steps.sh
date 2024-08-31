685d72cdbe1b45a4231f4165c9f2f5022c5df6a3
Richard Anderson <46491031+richardjanderson9@users.noreply.github.com>
2024-08-31 21:18:04 +0100

    Update first-steps.sh

--- first-steps.sh
+++ first-steps.sh
@@ -3,8 +3,11 @@
 # Create the hello-world.txt file in /root
 echo "Hello, World!" > /root/hello-world.txt
 
-# Update the default Apache2 index.html file
-echo "This script has worked." > /var/www/html/index.html
+# Get the current time in the desired format
+current_time=$(date +'%d/%m/%y @ %H:%M:%S')
+
+# Update the default Apache2 index.html file with dynamic content
+echo "The script has worked! (Time of change: $current_time)" > /var/www/html/index.html
 
 # Restart the Apache2 service
 systemctl restart apache2
