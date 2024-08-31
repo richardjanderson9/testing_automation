e2f1fbab69f80d7c26e7e65129cdfe497c3cf314
Richard Anderson <46491031+richardjanderson9@users.noreply.github.com>
2024-08-31 23:01:57 +0100

    Update first-steps.sh

--- first-steps.sh
+++ first-steps.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Create the hello-world.txt file in /root
-echo "Hello, World!" > /root/hello-world.txt
-
-# Get the current time in the desired format
+# Get the current time 
 current_time=$(date +'%d/%m/%y @ %H:%M:%S')
 
-# Update the default Apache2 index.html file with dynamic content
+# Create the hello-world.txt file and append the current time
+echo "Hello, World!\n$current_time" > /root/hello-world.txt
+
+# Update the default Apache2 index.html file with the current time
 echo "The script has worked! (Time of change: $current_time)" > /var/www/html/index.html
 
 # Restart the Apache2 service
 systemctl restart apache2
+
+# Pull the "hello-world" Docker image
+docker pull hello-world
+
+# Run the "hello-world" container three times
+for i in {1..3}; do
+    docker run hello-world
+done
