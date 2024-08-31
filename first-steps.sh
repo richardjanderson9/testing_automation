bcdd37b4bc00385a5b0e0a2ec1d97271d98f5ce6
Richard Anderson <46491031+richardjanderson9@users.noreply.github.com>
2024-08-31 20:52:12 +0100

    Update first-steps.sh

--- first-steps.sh
+++ first-steps.sh
@@ -2,3 +2,9 @@
 
 # Create the hello-world.txt file in /root
 echo "Hello, World!" > /root/hello-world.txt
+
+# Update the default Apache2 index.html file
+echo "This script has worked." > /var/www/html/index.html
+
+# Restart the Apache2 service
+systemctl restart apache2
