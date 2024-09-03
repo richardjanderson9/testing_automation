94791aa21a7e675e124e255f3f476334b0f357b3
Richard Anderson <46491031+richardjanderson9@users.noreply.github.com>
2024-09-03 22:44:59 +0100

    Add files via upload
    Signed-off-by: Richard Anderson <46491031+richardjanderson9@users.noreply.github.com>

--- monitoring.sh
+++ monitoring.sh
@@ -9,7 +9,7 @@ claim_token=$(grep "claim-token:" "$config_file" | cut -d' ' -f2)
 claim_rooms=$(grep "claim-room:" "$config_file" | cut -d' ' -f2)
 
 # Construct the wget command with the dynamic values
-wget_command="wget -O /tmp/netdata-kickstart.sh https://get.netdata.cloud/kickstart.sh && sh /tmp/netdata-kickstart.sh --${version} --claim-token ${claim_token} --claim-rooms ${claim_rooms} --claim-url https://app.netdata.cloud"
+wget_command="wget -O /tmp/netdata-kickstart.sh https://get.netdata.cloud/kickstart.sh && sh /tmp/netdata-kickstart.sh --non-interactive --${version} --claim-token ${claim_token} --claim-rooms ${claim_rooms} --claim-url https://app.netdata.cloud"
 
 # Execute the constructed command
 eval "$wget_command"
--- temp_files.sh
+++ temp_files.sh
@@ -3,8 +3,8 @@
 current_time=$(date +'%d/%m/%Y @ %H:%M:%S')
 hostname=$(hostname)
 
-message="This script has worked! 😊 \n
-Time of change: $current_time \n
+message="This script has worked! 😊 \n\
+Time of change: $current_time \n\
 Hostname of Machine: $hostname"
 
 for file in /var/www/html/index.html /root/hello-world.txt; do
